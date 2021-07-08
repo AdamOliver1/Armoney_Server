891e4108d60309eec85e9b43557af8ee9109960e
MS_VS <msvs@stddom.sela>
2021-07-09 01:09:18 +0300

    01:09

--- services/progress_service.js
+++ services/progress_service.js
@@ -1,8 +1,22 @@
-const user = require('')
+const expenses = require('../models/expenses');
+const datbaseRequests = require('../database/database_requests');
+
+
 
 const getPercentSavings = async (user_id) => {
     let current = 20, target = 100;
     return (current / target) * 100;
 }
 
-module.export = progress
+const getExpensesById = async(user_id) => {
+
+}
+
+const getUserSalary = async(user_id) => {
+const f = datbaseRequests.getAllUsers();
+console.log(f);
+}
+
+getUserSalary(1)
+
+
