18f831f5677d94ab01b90a3d6249ec30c1b06e1c
MS_VS <msvs@stddom.sela>
2021-07-08 21:39:40 +0300

    lesson suggestions services

---
+++ services/lesson_service.js
@@ -0,0 +1,26 @@
+
+
+class Lesson_service {
+    constructor() {
+
+        this.LessonsInvestingLongTerm = [
+            "https://www.youtube.com/watch?v=3KAsQJgvgh0",
+            'https://www.youtube.com/watch?v=F4z6RDNYAwg'
+        ]
+        this.LessonsInvestingShortTerm = [
+            "https://www.hasolidit.com/",
+            'https://www.youtube.com/watch?v=FSlM5cp6Jys'
+        ]
+    }
+
+
+
+    getInvestingLongTerm(){
+        return this.LessonsInvestingLongTerm;
+    }
+    getInvestingShortTerm = () => {
+        return this.LessonsInvestingShortTerm;
+    }
+
+}
+module.exports = new Lesson_service();
---
+++ services/suggestion_service.js
@@ -0,0 +1,23 @@
+
+class suggestion_service {
+    constructor() {
+
+        this.mashkarCard = 'https://www.mashkar.co.il/%D7%9B%D7%A8%D7%98%D7%99%D7%A1%D7%99%D7%9D-%D7%A0%D7%98%D7%A2%D7%A0%D7%99%D7%9D';
+        this.InvestmentsTools = {
+           'קופת גמל': "https://campaign-1.as-invest.co.il/gemel/?src=google&utm_source=google&utm_medium=cpc&utm_campaign=klg_altshuler_shaham_gemel_gemel-invest_generic_search_t4323&utm_term=%2B%D7%92%D7%9E%D7%9C%20%2B%D7%9C%D7%94%D7%A9%D7%A7%D7%A2%D7%94&gclid=EAIaIQobChMI2_jc2IXU8QIVCyIYCh3zlAqIEAAYASAAEgIGS_D_BwE",
+           'בתי השקעות': 'https://www.duns100.co.il/rating/%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D_%D7%A4%D7%99%D7%A0%D7%A0%D7%A1%D7%99%D7%99%D7%9D/%D7%91%D7%AA%D7%99_%D7%94%D7%A9%D7%A7%D7%A2%D7%95%D7%AA'
+        }
+    }
+
+
+
+    getMashkarCard = () => {
+        return this.mashkarCard;
+    }
+    getInvestingShortTerm = () => {
+        return this.LessonsInvestingShortTerm;
+    }
+
+}
+
+module.exports = new suggestion_service;
