cbb97235c0bed59d864c3d423ad5a8dbcf38f7c4
MS_VS <msvs@stddom.sela>
2021-07-08 23:56:23 +0300

    working add

--- database/database_requests.js
+++ database/database_requests.js
@@ -1,21 +1,21 @@
 const db = require('../configs/db_config');
 
 class DatbaseRequests {
-    getAllUsers = async () => {
+    async getAllUsers (){
         try{
         const users = await db.users.findAll();
-        return users;
+        return JSON.stringify(users);
         }catch(error){
             console.log(error);
             return [];
         }
     }
 
-    addUser = (props) => {
+    async addUser (props) {
         try {
             const newUser = await db.users.create({ email: props.email, password: props.password, phone: props.phone, first_name: props.first_name, last_name: props.last_name });
             const moneyData = await db.money_data.create({ money_target: props.money_target, current_money: props.current_money });
-            const newSoldier = await db.soldiers.create({ soldier_type: soldier_type, soldier_tash_type: soldier_tash_type, money_addons: money_addons, additional_income: additional_income, days_home: days_home, user_id: newUser.user_id, is_lone_soldier: props.is_lone_soldier, money_data_id: moneyData.money_data_id });
+            const newSoldier = await db.soldiers.create({ soldier_type: props.soldier_type, soldier_tash_type: props.soldier_tash_type, money_addons: props.money_addons, additional_income: props.additional_income, days_home: props.days_home, user_id: newUser.user_id, is_lone_soldier: props.is_lone_soldier, money_data_id: moneyData.money_data_id });
             return true;
         } catch (error) {
             console.log(error);
