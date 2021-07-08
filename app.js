5361fcb0b528f5c9e1cdff5433864073c55147ca
AdamOliver1 <adamoliver6929@gmail.com>
2021-07-08 18:31:38 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {lesson_service,progress_service,sign_up_service,suggestion_service} = require('./imports');
+const config = require('./configs/config');
+const app = express();
+app.use(sign_up_service);
+app.use(lesson_service);
+app.use(progress_service);
+app.use(suggestion_service);
+
+app.listen(config.PORT,() => {
+    console.log("ARMONEY IS IN YOUT POCKET!");
+    
+});
---
+++ imports.js
@@ -0,0 +1,12 @@
+const lesson_service = require('./services/lesson_service');
+const progress_service = require('/services/progress_service');
+const sign_up_service = require('/services/sign_up_service');
+const suggestion_service = require('/services/suggestion_service');
+
+
+export {
+    lesson_service,
+    progress_service,
+    sign_up_service,
+    suggestion_service
+}
