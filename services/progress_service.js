4ea6539e51a70791e6b9ac728a0dec90bccaf3b4
MS_VS <msvs@stddom.sela>
2021-07-09 00:07:23 +0300

    hello

--- services/progress_service.js
+++ services/progress_service.js
@@ -1,6 +1,8 @@
-const progress = {
+const user = require('')
 
-
+const getPercentSavings = async (user_id) => {
+    let current = 20, target = 100;
+    return (current / target) * 100;
 }
 
 module.export = progress
