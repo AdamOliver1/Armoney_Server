c591fbb982fcf8d9db172bdedcd46c843b2814a5
MS_VS <msvs@stddom.sela>
2021-07-09 03:59:41 +0300

    expenses

--- services/progress_service.js
+++ services/progress_service.js
@@ -1,6 +1,23 @@
-const progress = {
+const expenses = require('../models/expenses');
+const datbaseRequests = require('../database/database_requests');
 
 
+
+const getPercentSavings = async (user_id) => {
+   const {current_money,money_target} = datbaseRequests.getMoneyDataOfUser({"user_id":user_id})   
+    return (current_money / money_target) * 100;
 }
 
-module.export = progress
+const getExpensesById = async(user_id) => {
+ const expenses = await datbaseRequests.getAllUserExpenses({"user_id":user_id});
+ return expenses;
+}
+
+const getSavingsPercentById = async(user_id) => {
+    const expenses = getExpensesById(user_id);
+   return expenses.map(e => (e.army_mandatory+e.army_mandatory+e.home_pleasure+e.home_mandatory)/e.savings);
+   }
+
+
+
+c
